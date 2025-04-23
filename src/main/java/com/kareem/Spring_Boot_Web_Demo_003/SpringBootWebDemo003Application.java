035d5dde4a75302c548c25101bec1ff19ec6e3e8
Kareem Emad <@gmail.com>
2025-04-23 19:10:46 +0200

    Initial commit

---
+++ src/main/java/com/kareem/Spring_Boot_Web_Demo_003/SpringBootWebDemo003Application.java
@@ -0,0 +1,13 @@
+package com.kareem.Spring_Boot_Web_Demo_003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebDemo003Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebDemo003Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kareem/Spring_Boot_Web_Demo_003/SpringBootWebDemo003ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kareem.Spring_Boot_Web_Demo_003;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebDemo003ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
